40c2bd8afa806d6fc11cb6cadf2169ab94a8f91b
yongjunhong <kevin0928@naver.com>
2024-10-20 14:58:48 +0900

    feat : generate gemini api

--- src/main/java/TripDesigners/Backend/trip/controller/TripController.java
+++ src/main/java/TripDesigners/Backend/trip/controller/TripController.java
@@ -1,6 +1,8 @@
 package TripDesigners.Backend.trip.controller;
 
+import org.springframework.ai.vertexai.gemini.VertexAiGeminiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Getter;
@@ -10,8 +12,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class TripController {
 
+	private final VertexAiGeminiChatModel vertexAiGeminiChatModel;
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello, World!";
+	}
+
+	@GetMapping("/gemini")
+	public String gemini() {
+		return vertexAiGeminiChatModel.call("Hello, Gemini!");
+	}
+
+	@GetMapping("/trip/{comment}")
+	public String gemini(@PathVariable String comment) {
+		return vertexAiGeminiChatModel.call(comment);
 	}
 }
