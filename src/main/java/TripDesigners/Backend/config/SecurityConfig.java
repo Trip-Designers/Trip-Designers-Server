fbcc554111fb42d8dcaff6ae6e2e6b0cd08d60c8
Yongjun Hong <kevin0928@naver.com>
2024-10-20 21:12:48 +0900

    Feature/gemini (#5)
    * Add publish test results code
    
    * Fix release.yml
    
    * Fix push.yml
    
    * Fix push.yml
    
    * Fix push.yml
    
    * feat : generate gemini api
    
    * fix on push branches
    
    * change gemini to openai
    
    * fix cicd

---
+++ src/main/java/TripDesigners/Backend/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package TripDesigners.Backend.config;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+			.formLogin(AbstractHttpConfigurer::disable)
+			.build();
+	}
+}
--- src/main/java/TripDesigners/Backend/trip/controller/TripController.java
+++ src/main/java/TripDesigners/Backend/trip/controller/TripController.java
@@ -1,17 +1,25 @@
 package TripDesigners.Backend.trip.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class TripController {
 
+	private final OpenAiChatModel openAiChatModel;
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello, World!";
+	}
+
+	@GetMapping("/gemini")
+	public String gemini() {
+		return openAiChatModel.call("Hello, Gemini!");
 	}
 }
